8aa3979155c306d215a4a3effb6dfe275530a4aa
fathiyarn <89031957+fathiyarn@users.noreply.github.com>
2026-07-14 15:48:08 +0700

    Update ContactTest.java

--- src/test/java/ContactTest.java
+++ src/test/java/ContactTest.java
@@ -24,9 +24,12 @@ public class ContactTest extends BaseTest {
 
 		// Add explicit wait for the success alert to appear
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		WebElement alert = wait.until(
-			ExpectedConditions.visibilityOfElementLocated(contactPagePom.alertLoc)
-		);
+
+		wait.until(ExpectedConditions.visibilityOfElementLocated(firstNameLoc)).sendKeys("John");
+		
+		// WebElement alert = wait.until(
+		// 	ExpectedConditions.visibilityOfElementLocated(contactPagePom.alertLoc)
+		// );
 		String alertText = alert.getText().trim();
 
 		assertEquals("Thanks for your message! We will contact you shortly.", alertText);
