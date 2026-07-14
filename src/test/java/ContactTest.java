01d5603022b3bd9a6fbdbff28212234c7605f021
fathiyarn <89031957+fathiyarn@users.noreply.github.com>
2026-07-14 15:11:55 +0700

    Update ContactTest.java
    to find an error about ContactTest.testContactFormSubmit:20 » NoSuchElement no such element: Unable to locate element: {"method":"css selector","selector":"#first_name"}

--- src/test/java/ContactTest.java
+++ src/test/java/ContactTest.java
@@ -1,18 +1,36 @@
 import org.junit.jupiter.api.Test;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
+
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ContactTest extends BaseTest {
 	@Test
-	public void testContactFormSubmit() {
+	public void testContactFormSubmit throws Exception() {
 		driver.get("https://practicesoftwaretesting.com/contact");
+
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getTitle());
+		
+		Thread.sleep(5000);
+		
+		File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		Files.copy(
+		    screenshot.toPath(),
+		    Path.of("contact.png"),
+		    StandardCopyOption.REPLACE_EXISTING
+		);
 
 		ContactPage contactPagePom = new ContactPage(driver);
 
